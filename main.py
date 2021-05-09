c658ce8cda47a2237fd6c0de0224f05f37256f9a
tjthejuggler <tjthejuggler@gmail.com>
2021-05-09 13:13:09 -0400

    closed windows can return data

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import tkinter.colorchooser
 import tkinter.filedialog
 from mutagen.oggvorbis import OggVorbis
 import numpy as np
-#import pygame
 import os
 from os import path
 from pathlib import Path
@@ -11,39 +10,27 @@ import json
 import colour
 import mbox
 
+#root = tkinter.Tk()
+
+
 
 from music import *
 from timeline import *
 from ball import *
 
-
 home = str(Path.home())
 cwd = os.getcwd()
 
-default_colors = ["#000000",
-"#FFFFFF",
-"#FF0000",
-"#00FF00",
-"#0000FF",
-"#FFFF00",
-"#00FFFF",
-"#FF00FF",
-"#00FFFF",
-"#FF00FF"]
+default_colors = ["#000000","#FFFFFF","#FF0000","#00FF00","#0000FF","#FFFF00","#00FFFF","#FF00FF","#00FFFF","#FF00FF"]
 
 def main():
     canvas_width, canvas_height = 300, 300
-
-
     root = tkinter.Tk()
-
     Mbox = mbox.Mbox
     Mbox.root = root
-
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
     root.geometry("%dx%d+0+0" % (w, h))
     track_length = OggVorbis('output.ogg').info.length
-    #quit_button = tkinter.Button(root, command=root.quit, text="Quit").pack()
     lower_key_labels = [['1','2','3','4','5','6','7','8','9','0'],
                   ['q','w','e','r','t','y','u','i','o','p'],
                   ['a','s','d','f','g','h','j','k','l',';'],
@@ -54,65 +41,47 @@ def main():
                   ['Z','X','C','V','B','N','M','<','>','?']]
     current_colors = default_colors
 
+
+
+
+
+
     def keypress_handler(event):
-
-
-
-
-        print('foc',root.focus_get())
         if not 'spinbox' in str(root.focus_get()) and event.char:
-            print('event.char', event.char)
             fade = False
-                #when this is held we need to make the anotation for our dict that is "fade"
-                    #i think it just be another item with a proper timestamp that just says "fade"
-                #then show the updated timeline with a fade showing
             lower_array = np.array(lower_key_labels)
             lower_solutions = np.argwhere(lower_array == event.char)
             if lower_solutions.size > 0:
                 solutions = lower_solutions
             upper_array = np.array(upper_key_labels)
             upper_solutions = np.argwhere(upper_array == event.char)
-            print('upper_solutions', upper_solutions)
             if upper_solutions.size > 0:
-                print('upper_solutions.size', upper_solutions.size)
                 solutions = upper_solutions
                 fade = True
-            print('solutions1', solutions)
-            print('fade', fade)
             music.test_print()
             if solutions.size > 0:
                 key_row = solutions[0][0]
                 key_column = solutions[0][1]
                 cur_slider_time = music.slider.get()
                 key_color = keys[key_row][key_column]['bg']
-                print('key_row', key_row)
                 if fade:
-                    #print('fade')
                     previousColor = music.getCurrentBallColor( key_row )#i need a previous time as well to determine to fade timestamps
-                    #print('previousColor', previousColor)
-                    fade_levels = 100
-                    fade_colors = list(colour.Color(previousColor).range_to(colour.Color(key_color),fade_levels))
                     previousMarkerTime = timelines[key_row].most_recent_marker_time
                     fade_gap_length = cur_slider_time - previousMarkerTime
+                    print('fade_gap_length', fade_gap_length)
+                    fade_levels = max(int(fade_gap_length)*2, 50)
+                    fade_colors = list(colour.Color(previousColor).range_to(colour.Color(key_color),fade_levels))
                     gap_increment = fade_gap_length / fade_levels
                     for index, fade_color in enumerate(fade_colors):
-
-                        #print('key_color', key_color)
-                        #print('control held', fade_color)
-                        #print('dasd', previousMarkerTime + (index * gap_increment))
-                        #print('cur_slider_time', cur_slider_time)
                         timelines[key_row].add_marker(previousMarkerTime + (index * gap_increment), fade_color.hex_l, music.slider_width)
                         music.update_timeline_markers(timelines[key_row].marker_data,key_row)
                 else:
                     timelines[key_row].add_marker(cur_slider_time, key_color, music.slider_width)
                     music.update_timeline_markers(timelines[key_row].marker_data,key_row)
-                print('finished')
-        print('super')
 
     def file_load():
         chosen_dir = tkinter.filedialog.askdirectory()
         if chosen_dir is None:
-            print("file_load return")
             return
         for i in range(4):
             if path.exists(chosen_dir+'/marker_data'+str(i)):         
@@ -136,11 +105,8 @@ def main():
 
     save_button = tkinter.Button(root, command=file_save, text="Save").pack()
     load_button = tkinter.Button(root, command=file_load, text="Load").pack()
-
-
     event_sequence = '<KeyPress>'
-    root.bind(event_sequence, keypress_handler)
-    
+    root.bind(event_sequence, keypress_handler)    
     timelines = [None]*4
     for i in range(4):        
         timelines[i] = TimeLine( root, i, track_length, w)
@@ -157,13 +123,29 @@ def main():
         ass = np.array(keys)
         print(keys[i])
 
+    def testtest( self ):
+        print('testtest')
+
     def right_click( self ):
         print('right_click')#this right click is where we show the mini editor
         #root.grab_set()
+
         D = {'user':'Bob'}
 
-        Mbox('Name?', (D, 'user'))
-        
+        print('D', D)
+        # b_login = tkinter.Button(root, text='Log in')
+        # b_login['command'] = lambda: Mbox('Name?', (D, 'user'))
+        # b_login.pack()
+
+        # b_loggedin = tkinter.Button(root, text='Current User')
+        # b_loggedin['command'] = lambda: Mbox(D['user'])
+        # b_loggedin.pack()
+        test = Mbox(root, "testing").show()
+        #test.bind("<Destroy>",testtest())
+        print('test.result333', test.result)
+
+
+        print('D2', D)      
 
     key_button_frame = tkinter.Frame(root, 
                width=canvas_width, 
@@ -183,6 +165,7 @@ def main():
 
     root.mainloop()
 
-
 if __name__ == '__main__':
     main()
+
+    
--- mbox.py
+++ mbox.py
@@ -4,39 +4,54 @@ class Mbox(object):
 
     root = None
 
-
     def __init__(self, msg, dict_key=None):
         """
         msg = <str> the message to be displayed
         dict_key = <sequence> (dictionary, key) to associate with user input
         (providing a sequence for dict_key creates an entry for user input)
         """
-        root = tkinter.Tk()
-        w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-        root.geometry("%dx%d+0+0" % (w, h))
-        #self.top = tkinter.Toplevel(Mbox.tki)
-        #root.grab_set()
-        frm = tkinter.Frame(root, borderwidth=4, relief='ridge')
+        tki = tkinter
+        self.top = tki.Toplevel(Mbox.root)
+
+        frm = tki.Frame(self.top, borderwidth=4, relief='ridge')
         frm.pack(fill='both', expand=True)
 
-        label = tkinter.Label(frm, text=msg)
+        label = tki.Label(frm, text=msg)
         label.pack(padx=4, pady=4)
 
+        caller_wants_an_entry = dict_key is not None
 
-        self.entry = tkinter.Entry(frm)
+        #if caller_wants_an_entry:
+        self.entry = tki.Entry(frm)
         self.entry.pack(pady=4)
+        self.result = None
 
-        b_submit = tkinter.Button(frm, text='Submit')
+        b_submit = tki.Button(frm, text='Submit')
         b_submit['command'] = lambda: self.entry_to_dict(dict_key)
         b_submit.pack()
 
-        b_cancel = tkinter.Button(frm, text='Cancel')
-        b_cancel['command'] = root.destroy
+        b_cancel = tki.Button(frm, text='Cancel')
+        b_cancel['command'] = self.top.destroy
         b_cancel.pack(padx=4, pady=4)
 
     def entry_to_dict(self, dict_key):
+        print('dict_key', dict_key)
         data = self.entry.get()
+        print('data', data)
         if data:
-            d, key = dict_key
-            d[key] = data
-            root.destroy()
+            #d, key = dict_key
+            #d[key] = data
+            #self.top.destroy()
+            self.result = data
+
+    def show( self ):
+        #self.wm_deiconify()
+        self.entry.focus_force()
+        self.top.wait_window()
+        return self
+
+        #put this into texteditor
+        #make apply close the window too, make cancel not pass anything
+        #change this into a mini editor
+        #change this name
+        #rename other things
